4635fed1ccdccd5a436888e211b4efde2db49c6c
Gav Hanna <gavhanna@gmail.com>
2017-03-29 17:30:44 +0100

    changed page layout

---
+++ js/main.js
@@ -0,0 +1,15 @@
+(function(){
+  if (window.matchMedia("(min-width: 500px)").matches) {
+    $(".gallery").justifiedGallery({
+      rowHeight : 270,
+      lastRow : 'justify',
+      margins : 3
+    });
+  } else {
+    $(".gallery").justifiedGallery({
+      rowHeight : 170,
+      lastRow : 'justify',
+      margins : 3
+    });
+  }
+})()
